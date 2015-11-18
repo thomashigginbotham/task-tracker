1f976a4de33d4aca262b00ecf6b87e4a74d29a66
Thomas Higginbotham <thomas.higginbotham@experis.com>
2015-11-18 08:44:48 -0500

    Allow menu items to open notification block

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -6,44 +6,20 @@ define(['jquery'], function($) {
 	 * Initialization
 	 */
 	var init = function() {
-		// ES6
-		require(['system'], function(System) {
-			System.config({
-				defaultJSExensions: true
-			});
-		});
-
-		// Site Footer scripts
-		require(['footer'], function(footer) {
-			footer.addText('This text was added with JavaScript. See <strong>/app/javascripts/app.js</strong> to make changes.');
-		});
+		
 	};
 
 	/*
 	 * Main entry point
 	 */
-	(function() {
-		// Allow XPL to re-run start-up scripts when pattern library components
-		// have finished loading
-		if (window.addEventListener) {
-			window.addEventListener('xplComponentsLoaded', function() {
-				setTimeout(init, 0);
-			}, false);
-		}
+	init();
 
-		// Let's go!
-		setTimeout(function() {
-			init();
+	// Allow XPL to re-run start-up scripts when pattern library components
+	// have finished loading
+	if (window.addEventListener) {
+		window.addEventListener('xplComponentsLoaded', function() {
+			setTimeout(init, 0);
+		}, false);
+	}
 
-			// Use Selectivizr and Respond.js for IE 8 and below
-			// IMPORTANT: Selectivizr requires a global jQuery object to work.
-			if ($('.lt-ie9').length > 0) {
-				var selectivizrPath = '/javascripts/vendor/selectivizr.js';
-
-				$.getScript(selectivizrPath, function() {
-					$.getScript('//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.js');
-				});
-			}
-		}, 0);
-	})();
 });
